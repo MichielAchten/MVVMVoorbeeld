0563a86d935be63cc3467a37f2c79af3ddf4e0cf
MichielAchten <michiel.achten@gmail.com>
2016-06-22 15:58:29 +0200

    Commit2

---
+++ MVVMHobby/ViewModel/HobbyLijstVM.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+using System.Windows;
+
+namespace MVVMHobby.ViewModel
+{
+    public class HobbyLijstVM: ViewModelBase
+    {
+        public HobbyLijstVM()
+        {
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("sport", "voetbal", new BitmapImage(new Uri("pack://application:,,,/images/voetbal.jpg", UriKind.Absolute)))));
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("sport", "atletiek", new BitmapImage(new Uri("pack://application:,,,/images/atletiek.jpg", UriKind.Absolute)))));
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("sport", "basketbal", new BitmapImage(new Uri("pack://application:,,,/images/basketbal.jpg", UriKind.Absolute)))));
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("sport", "tennis", new BitmapImage(new Uri("pack://application:,,,/images/tennis.jpg", UriKind.Absolute)))));
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("sport", "turnen", new BitmapImage(new Uri("pack://application:,,,/images/turnen.jpg", UriKind.Absolute)))));
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("muziek", "trompet", new BitmapImage(new Uri("pack://application:,,,/images/trompet.jpg", UriKind.Absolute)))));
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("muziek", "drum", new BitmapImage(new Uri("pack://application:,,,/images/drum.jpg", UriKind.Absolute)))));
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("muziek", "gitaat", new BitmapImage(new Uri("pack://application:,,,/images/gitaat.jpg", UriKind.Absolute)))));
+            HobbyLijst.Add(new HobbyVM(new Model.Hobby("muziek", "piano", new BitmapImage(new Uri("pack://application:,,,/images/piano.jpg", UriKind.Absolute)))));
+        }
+
+        private ObservableCollection<HobbyVM> hobbyLijstValue = new ObservableCollection<HobbyVM>();
+        public ObservableCollection<HobbyVM> HobbyLijst
+        {
+            get
+            {
+                return hobbyLijstValue;
+            }
+            set
+            {
+                hobbyLijstValue = value;
+                RaisePropertyChanged("HobbyLijst");
+            }
+        }
+
+        private HobbyVM selectedHobbyValue;
+        public HobbyVM SelectedHobby
+        {
+            get
+            {
+                return selectedHobbyValue;
+            }
+            set
+            {
+                selectedHobbyValue = value;
+                RaisePropertyChanged("SelectedHobby");
+            }
+        }
+
+        public RelayCommand<RoutedEventArgs> VerwijderCommand
+        {
+            get
+            {
+                return new RelayCommand<RoutedEventArgs>(Verwijder);
+            }
+        }
+
+        private void Verwijder(RoutedEventArgs e)
+        {
+            HobbyLijst.Remove(SelectedHobby);
+        }
+    }
+}
---
+++ MVVMHobby/ViewModel/HobbyVM.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+
+//using System.Windows;
+using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+
+namespace MVVMHobby.ViewModel
+{
+    public class HobbyVM: ViewModelBase
+    {
+        private Model.Hobby hobby;
+        public HobbyVM(Model.Hobby nHobby)
+        {
+            this.hobby = nHobby;
+        }
+
+        public string Categorie
+        {
+            get
+            {
+                return hobby.Categorie;
+            }
+            set
+            {
+                hobby.Categorie = value;
+                RaisePropertyChanged("Categorie");
+            }
+        }
+
+        public string Activiteit
+        {
+            get 
+            { 
+                return hobby.Activiteit; 
+            }
+            set 
+            { 
+                hobby.Activiteit = value;
+                RaisePropertyChanged("Activiteit");
+            }
+        }
+
+        public BitmapImage Symbool
+        {
+            get
+            {
+                return hobby.Symbool;
+            }
+            set
+            {
+                hobby.Symbool = value;
+                RaisePropertyChanged("Symbool");
+            }
+        }
+
+        View.ImageView groteView;
+
+        public RelayCommand<MouseEventArgs> MouseDownCommand
+        {
+            get
+            {
+                return new RelayCommand<MouseEventArgs>(MuisIn);
+            }
+        }
+
+        private void MuisIn(MouseEventArgs e)
+        {
+            Image tg = (Image)e.OriginalSource;
+            groteView = new View.ImageView();
+            //groteView.GroteImage.source
+            //nog niet klaar
+        }
+    }
+}
