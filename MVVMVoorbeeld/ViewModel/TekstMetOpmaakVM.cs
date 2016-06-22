1574cde75786d56d5a7ed514743abb78f678583a
MichielAchten <michiel.achten@gmail.com>
2016-06-22 10:11:00 +0200

    Commit1

---
+++ MVVMVoorbeeld/ViewModel/TekstMetOpmaakVM.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MVVMVoorbeeld.ViewModel
+{
+    public class TekstMetOpmaakVM: ViewModelBase
+    {
+        private Model.TekstMetOpmaak opgemaakteTekst;
+
+        public TekstMetOpmaakVM(Model.TekstMetOpmaak deTekst)
+        {
+            opgemaakteTekst = deTekst;
+        }
+
+        public string Inhoud
+        {
+            get
+            {
+                return opgemaakteTekst.Inhoud;
+            }
+            set
+            {
+                opgemaakteTekst.Inhoud = value;
+                RaisePropertyChanged("Inhoud");
+            }
+        }
+
+        public Boolean Vet
+        {
+            get
+            {
+                return opgemaakteTekst.Vet;
+            }
+            set
+            {
+                opgemaakteTekst.Vet = value;
+                RaisePropertyChanged("Vet");
+            }
+        }
+
+        public Boolean Schuin
+        {
+            get
+            {
+                return opgemaakteTekst.Schuin;
+            }
+            set
+            {
+                opgemaakteTekst.Schuin = value;
+                RaisePropertyChanged("Schuin");
+            }
+        }
+    }
+}
